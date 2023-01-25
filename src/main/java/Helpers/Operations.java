9c3924310c5f8401e1c1fa621b498506128b2451
Karalina199 <carolina.chernenok@gmail.com>
2023-01-25 16:48:04 +0300

    Refactor calculator

---
+++ src/main/java/Helpers/Operations.java
@@ -0,0 +1,20 @@
+package Helpers;
+
+public class Operations {
+
+    public static long addition (int firstNum, int secondNum) {
+         return (long) firstNum + (long) secondNum;
+    }
+
+    public static int substract (int firstNum, int secondNum) {
+        return firstNum - secondNum;
+    }
+
+    public static long multiplication (int firstNum, int secondNum) {
+        return (long) firstNum * (long) secondNum;
+    }
+
+    public static double divide (int firstNum, int secondNum) {
+        return (double) firstNum / (double) secondNum;
+    }
+}
--- src/main/java/SimpleCalculator.java
+++ src/main/java/SimpleCalculator.java
@@ -1,61 +1,13 @@
-import java.util.Scanner;
+import Helpers.Actions;
+import Helpers.TextHelper;
 
 public class SimpleCalculator {
-    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        double fistNum = getNumber();
-        char operation = getOperation();
-        double secondNum = getNumber();
-        double result = calculator(fistNum,secondNum,operation);
-        System.out.println("Your result is: " + result);
-    }
-
-    public static int getNumber() {
-        System.out.println("Enter an integer number:");
-        int number;
-        if (sc.hasNextInt()) {
-            number = sc.nextInt();
-        } else {
-            System.out.println("Incorrect value. Try again");
-            sc.next();
-            number=getNumber();
-        }
-        return number;
-    }
-
-    public static char getOperation() {
-        System.out.println("Enter an operation:");
-        char operation;
-        if (sc.hasNext()) {
-            operation=sc.next().charAt(0);
-        }else {
-            System.out.println("Invalid operation. Try again");
-            sc.next();
-            operation=getOperation();
-        }
-        return operation;
-    }
-
-    public static double calculator (double fistNum, double secondNum, char operation) {
-        double result;
-        switch (operation) {
-            case '+':
-                result = fistNum + secondNum;
-                break;
-            case '-':
-                result = fistNum - secondNum;
-                break;
-            case '*':
-                result = fistNum * secondNum;
-                break;
-            case '/':
-                result = fistNum / secondNum;
-                break;
-            default:
-                System.out.println("Invalid operation.Valid values are: \"+\", \"-\", \"*\", \"/\". Try again");
-                result = calculator(fistNum,secondNum,getOperation());
-        }
-        return result;
+            int fistNum = Actions.getFirstNumber();
+            int secondNum = Actions.getSecondNumber();
+            char operation = Actions.getOperation();
+            double result = Actions.calculator(fistNum, secondNum, operation);
+            System.out.println(TextHelper.RESULT_TEXT.getText() + result);
     }
 }
