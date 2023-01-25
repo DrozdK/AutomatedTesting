b26f5d681ac1a511d4f35fff3cc5f106377ef4f6
Karalina199 <carolina.chernenok@gmail.com>
2023-01-26 00:02:09 +0300

    Add check-list and Multiply package

---
+++ src/test/java/Multiply/MultiplyTests.java
@@ -0,0 +1,39 @@
+package Multiply;
+
+import Helpers.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MultiplyTests {
+
+    @Test
+    public void multiplicationMinTest() {
+        Assert.assertEquals((Actions.calculator(-2147483648, -2147483648, '*')), "4611686018427387904");
+    }
+
+    @Test
+    public void multiplicationWithAllPositiveNumbersTest() {
+        Assert.assertEquals((Actions.calculator(7, 7, '*')), "49");
+    }
+
+    @Test
+    public void multiplicationWithFirstPositiveNumberTest() {
+        Assert.assertEquals((Actions.calculator(7, -7, '*')), "-49");
+    }
+
+    @Test
+    public void multiplicationWithSecondPositiveNumberTest() {
+        Assert.assertEquals((Actions.calculator(-7, 7, '*')), "-49");
+    }
+
+    @Test
+    public void multiplicationWithAllNegativeNumbersTest() {
+        Assert.assertEquals((Actions.calculator(-7, -7, '*')), "49");
+    }
+
+    @Test
+    public void multiplicationMaxTest() {
+        Assert.assertEquals((Actions.calculator(2147483647, 2147483647, '*')), "4611686014132420609");
+    }
+
+}
